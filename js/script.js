5d32482874d14fe33985abd9265f39349ccfaf25
hip1310 <harshpatel131090@gmail.com>
2017-10-22 09:35:47 -0400

    Load NYTimes articles based on location input from user

--- js/script.js
+++ js/script.js
@@ -22,11 +22,46 @@ function loadData() {
     var location = street + ', ' + city;
     $greeting.text('So, you want to live at ' + location + '?');
 
-    var imgUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location='
+    var imgUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='
                 + location;
 
     // Set the street view image on background
     $body.append('<img class="bgimg" src="' + imgUrl + '">');
+
+    // Load the NYTimes artices based on the location
+    $nytHeaderElem.text('New York Times Articles for ' + city);
+
+    // NyTimes REST API base url
+    var nytimesurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+    // Use jquery's getJSON() method to send AJAX request and get
+    // NYTimes articles
+    // .getJSON() is simply a wrapper around .ajax() but it provides a
+    // simpler method signature as some of the settings are defaulted
+    // e.g dataType to json, type to get etc
+    $.getJSON( nytimesurl,
+      {
+        // Specify all the query parameters here for GET request
+        'api-key' : '<enter-your-api-key-for-nytimes>',
+        'q'       : location,
+        'sort'    : 'newest'
+      })
+      .done(function(json) {
+        // Looping through all the docs received in json response
+        $.each( json.response.docs, function( i, item ) {
+            // Insert list element for each NYTimes article
+            // Include url and snippet of the article
+            $nytElem.append('<li class="article">' +
+                             '<a href="' + item.web_url + '">'
+                             + item.headline.main + '</a>'
+                             + '<p>' + item.snippet + '</p></li>');
+        });
+      })
+      .fail(function( jqxhr, textStatus, error ) {
+        // Provide a failure/error message here
+        $nytHeaderElem.text('New York Times Articles could not be loaded');
+      }
+    );
 
     return false;
 };
